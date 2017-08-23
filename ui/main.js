961ac3f33e424aa31053dfcf202b437d1ef03f09
Aaditya1908 <31135060+Aaditya1908@users.noreply.github.com>
2017-08-23 21:46:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 // Change the text of the element div
 var element=document.getElementById('main-text');
 element.innerHTML='new value';
+//move the image
+var img=document.getElementById('pic');
+img.onclick=function()
+{
+    img.style.marginleft='100px';
+}
